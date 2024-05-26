35a90cf700fcb6f78d9d9d61e429dd7ba4ba93f2
Federico Nicolas Loiero <fede_loi95@hotmail.com>
2024-05-26 19:43:49 -0300

    cambios generales en el repo y actualizacion de game time

---
+++ Game Time/js/api.js
@@ -0,0 +1,35 @@
+const url = 'https://free-to-play-games-database.p.rapidapi.com/api/games';
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'a3d2898a84msh27d2a272d88fcd8p13c7cajsn5c64c8b3b68a',
+        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+    }
+};
+
+async function getGames() {
+    try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+        renderGames(data.slice(0, 20));
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function renderGames(data) {
+    let rows = '';
+    for (let juego of data) {
+        rows += `
+        <div class="imagen_juego">
+          <img src="${juego.thumbnail}" alt="" class="borde-img" />
+          <p class="text-center text-dark" style="background-color: #ffce7a">
+            ${juego.title}
+          </p>
+        </div>
+        `;
+    }
+    document.querySelector('[data-ul]').innerHTML = rows;
+}
+
+getGames();
